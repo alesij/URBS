a45a0e10b3971225a84231076360127301c35239
Andrea capone <Capone.andrea195@gmail.com>
2020-06-19 17:56:27 +0200

    added first http request

---
+++ URBS/Controller/LoginController.swift
@@ -0,0 +1,11 @@
+//
+//  LoginController.swift
+//  URBS
+//
+//  Created by Andrea Capone on 19/06/2020.
+//  Copyright © 2020 Alessio Di Matteo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
